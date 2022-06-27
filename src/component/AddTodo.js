69ab5b278572a6137895c2de1c12acb2a757977e
Sebastian Metellus <65518883+smetellus01@users.noreply.github.com>
2022-06-26 22:10:56 -0400

    Update AddTodo.js

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Button, TextField } from "@mui/material";
+import { DesktopDatePicker , LocalizationProvider} from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 class AddTodo extends Component {
   // Create a local react state of the this component with both content date property set to nothing.
@@ -7,7 +9,8 @@ class AddTodo extends Component {
     super();
     this.state = {
       content: "",
-      date: ""
+      date: "",
+      due: null
     };
   }
   // The handleChange function updates the react state with the new input value provided from the user and the current date/time.
@@ -17,6 +20,12 @@ class AddTodo extends Component {
     this.setState({
       content: event.target.value,
       date: Date().toLocaleString('en-US')
+    });
+  };
+
+  NewHandleChange = (event) => {
+    this.setState({
+      due: Date(event).toLocaleDateString()
     });
   };
   // The handleSubmit function collects the forms input and puts it into the react state.
@@ -29,7 +38,8 @@ class AddTodo extends Component {
       this.props.addTodo(this.state);
       this.setState({
         content: "",
-        date: ""
+        date: "",
+        due: null
       });
     }
   };
@@ -44,12 +54,24 @@ class AddTodo extends Component {
       // 4. The value of the text field also should reflect the local state of this component.
       <div>
         <TextField
+          data-testid = "new-item-input"
           label="Add New Item"
           variant="outlined"
           onChange={this.handleChange}
           value={this.state.content}
         />
-        <Button
+
+      <LocalizationProvider dateAdapter={AdapterDateFns}>         
+      <DesktopDatePicker
+       id="new-item-date"
+       label="Due Date"
+       value={this.state.due}
+       onChange={this.NewHandleChange}
+       renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
+
+      <Button
           style={{ marginLeft: "10px" }}
           onClick={this.handleSubmit}
           variant="contained"
@@ -63,3 +85,4 @@ class AddTodo extends Component {
 }
 
 export default AddTodo;
+
